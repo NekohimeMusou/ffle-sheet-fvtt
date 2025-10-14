de04d63fc4d60d250c23cfaf8f30368917702281
Nekohime <37454858+NekohimeMusou@users.noreply.github.com>
2025-10-13 23:55:48 -0400

    Fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,18 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig, globalIgnores } from "eslint/config";
 
+const FOUNDRY_GLOBALS = {
+  foundry: "readonly",
+  CONFIG: "readonly",
+  game: "readonly",
+};
+
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  globalIgnores("foundry/**/*")
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...FOUNDRY_GLOBALS } },
+  },
+  globalIgnores(["foundry/**/*"]),
 ]);
