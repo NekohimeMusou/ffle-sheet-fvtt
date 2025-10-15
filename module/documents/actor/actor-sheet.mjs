ab5a74dfcf34fb6fdc13bfcb6c6e8ed7abf7bd7e
Nekohime <37454858+NekohimeMusou@users.noreply.github.com>
2025-10-15 18:57:01 -0400

    Sheet works, visually

--- module/documents/actor/actor-sheet.mjs
+++ module/documents/actor/actor-sheet.mjs
@@ -22,16 +22,42 @@ export default class FFLEActorSheet extends HandlebarsApplicationMixin(ActorShee
     tabs: { template: "templates/generic/tab-navigation.hbs" },
     debug: {
       template: "systems/ffle-sheet/templates/sheets/actor/tabs/debug.hbs",
+      scrollable: [""],
     },
     notes: {
       template: "systems/ffle-sheet/templates/sheets/actor/tabs/notes.hbs",
+      scrollable: [""],
+    },
+  };
+
+  static TABS = {
+    primary: {
+      tabs: [{ id: "debug" }, { id: "notes" }],
+      labelPrefix: "FFLE.tab",
+      initial: "debug",
     },
   };
 
   async _prepareContext(_options) {
-    return {
+    const context = {
       FFLE,
       system: this.actor.system,
+      /** @type {Record<string, foundry.applications.types.ApplicationTab>} */
+      tabs: this._prepareTabs("primary"),
     };
+
+    return context;
+  }
+
+  async _preparePartContext(partId, context) {
+    switch (partId) {
+      case "debug":
+        break;
+      case "notes":
+        break;
+      default:
+    }
+
+    return context;
   }
 }
